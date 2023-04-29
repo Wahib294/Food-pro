95d53a54a86312f61bceac354364d8f99f0c339f
Wahib <96795579+Wahib294@users.noreply.github.com>
2023-04-29 14:57:30 +0500

    do

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,11 +36,40 @@ export default function IndexPage() {
 				<img src="/image1.png" alt="Image" className="max-h-full max-w-full" />
 			</div>
 			</div>
-			<div>
-				<div className="container flex flex-col">
-					</div>
+			<hr className='mx-10'/>
+			<div className="md:container flex md:mt-8 flex-col gap-9 md:flex-row">
+				<div className='mx-auto md:mx-0 md:flex md:gap-40'>	
+				<div className='flex my-5 md:my-0'>
+					<img src="./box1.png" alt=""  className='w-16 h-16'/>
+					<span className='my-auto'>
+					<h1 className='ml-3'>Free Shipping</h1>
+					<p className='text-xs ml-3 text-slate-600'>Orders over $140</p>
+					</span>
+				</div>
+				<div className='flex my-5 md:my-0'>
+					<img src="./box2.png" alt="" className='w-16 h-16'/>
+					<span className='my-auto'>
+					<h1 className='ml-3'>Quick Payment</h1>
+					<p className='text-xs ml-3 text-slate-600'>100% secure payment</p>
+					</span>
+				</div>
+				<div className='flex my-5 md:my-0'>
+					<img src="./box3.png" alt="" className='w-16 h-16'/>
+					<span className='my-auto'>
+					<h1 className='ml-3'>Special Promo</h1>
+					<p className='text-xs ml-3 text-slate-600'>Get special promo</p>
+					</span>
+				</div>
+				<div className='flex my-5 md:my-0'>
+					<img src="./box4.png" alt="" className='w-16 h-16'/>
+					<span className='my-auto'>
+					<h1 className='ml-3'>24/7 Support</h1>
+					<p className='text-xs ml-3 text-slate-600'>Ready Support</p>
+					</span>
+				</div>
 			</div>
-
+			</div>
+			<hr className='mx-10 md:my-6'/>
 		{/* <div className="container w-screen">
 			<ImageSlider images={images} />
 		</div> */}
